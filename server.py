37ba6216951befa54625d2c7c8edb618264c5904
KVIZORD <48475062+KVIZORD@users.noreply.github.com>
2020-04-08 15:48:44 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 import asyncio
+import datetime
 from asyncio import transports
 
-login_list = []
-message_list = []
+
+def get_time():
+    return datetime.datetime.now().strftime("%d-%m-%Y %H:%M")
 
 
 class ServerProtocol(asyncio.Protocol):
@@ -23,11 +25,12 @@ class ServerProtocol(asyncio.Protocol):
         else:
             if decoded.startswith('login:'):
                 login = decoded.replace('login:', '').replace("/r/n", "").replace(' ', '')
-                if login.lower() not in login_list:
+                if login.lower() not in self.server.login_list:
                     self.login = login
                     self.transport.write('Успешная регистрация\r\n'.encode())
-                    login_list.append(self.login.lower())
+                    self.server.login_list.append(self.login.lower())
                     self.send_history()
+                    print(f'({get_time()}) User {self.login} register')
                 else:
                     self.transport.write('Логин уже существует! Выберите другой\r\n'.encode())
             else:
@@ -35,35 +38,39 @@ class ServerProtocol(asyncio.Protocol):
 
     def send_message(self, content):
         message = f'{self.login}: {content}\r\n'.encode()
-        message_list.append(message.decode())
+        self.server.message_list.append(message.decode())
 
         for user in self.server.clients:
             user.transport.write(message)
 
     def send_history(self):
         last_message = 11
-        if len(message_list) < 10:
-            last_message = len(message_list)
+        if len(self.server.message_list) < 10:
+            last_message = len(self.server.message_list)+1
         self.transport.write('=======================\r\n'.encode())
-        for message in reversed(message_list[-1:-last_message:-1]):
+        for message in reversed(self.server.message_list[-1:-last_message:-1]):
             self.transport.write(f'{message}'.encode())
 
     def connection_made(self, transport: transports.Transport):
         self.server.clients.append(self)
         self.transport = transport
 
-        print('User connection')
+        print(f'({get_time()}) User connection')
 
     def connection_lost(self, exception):
         self.server.clients.remove(self)
-        print(f'User {self.login} disconnection')
+        print(f'({get_time()}) User {self.login} disconnection')
 
 
 class Server:
     clients: list
+    login_list: list
+    message_list: list
 
     def __init__(self):
         self.clients = []
+        self.login_list = []
+        self.message_list = []
 
     def build_protocol(self):
         return ServerProtocol(self)
